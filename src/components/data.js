cf66d262d8a3aa65d3314e6b3601d88c0d65dd19
Azeez <abolurinazeez2005@gmail.com>
2025-01-12 13:30:56 +0100

    first commit

---
+++ src/components/data.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    id: 0,
+    title: "Creamy Sazon Skillet Chicken",
+    image: "https://dummyimage.com/345x130/000/fff",
+    author: "By Mariana Fernandes",
+    prepTime: "10 min",
+    noOfIngredients: "10 ingredients",
+    mealType: "Healthy",
+    rating: "4.5",
+  },
+  {
+    id: 1,
+    title: "Italian Double Cheese Veggie Pizza",
+    author: "By Eleanor Voss",
+    image: "https://dummyimage.com/345x130/000/fff",
+    prepTime: "40 min",
+    noOfIngredients: "8 ingredients",
+    mealType: "Snacks",
+    rating: "4.5",
+  },
+  {
+    id: 2,
+    title: "Chicken Stir-Fry and Brocolli Mukbang",
+    author: "By Jackson Blythe",
+    image: "https://dummyimage.com/345x130/000/fff",
+    prepTime: "30 min",
+    noOfIngredients: "13 ingredients",
+    mealType: "Dessert",
+    rating: "4.5",
+  },
+];
+
+export default data;
+
+
+
+
+// author, recipe-title, image, rating, amount of ingredient, recipe-category, time for preparation,  
---
+++ src/components/hero-section.jsx
@@ -0,0 +1,38 @@
+import { useContext, useEffect } from "react";
+import RecipeCard from "./recipe-card";
+import { GlobalContext } from "./context";
+
+export default function HeroSections({ title, recipe }) {
+  const { favoriteList, setFavoriteList } = useContext(GlobalContext);
+
+  useEffect(() => {
+    const savedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    setFavoriteList(savedFavorites);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favoriteList));
+  }, [favoriteList]);
+  
+  return (
+    <section className="hero-section">
+      <h1>{title}</h1>
+      <div className="recipe-card-container">
+        {recipe.map((recipeItem) => (
+          <RecipeCard
+            key={recipeItem.id}
+            recipeItem={recipeItem}
+            isFavorite={favoriteList.includes(recipeItem.id)}
+            addToFavorite={(id) => {
+              setFavoriteList((prev) =>
+                prev.includes(id)
+                  ? prev.filter((favId) => favId !== id)
+                  : [...prev, id]
+              );
+            }}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
